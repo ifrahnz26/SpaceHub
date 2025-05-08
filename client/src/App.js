b5ee876cff24eeb7a96f71b63b04afb7e1d14eca
Ifrah Naaz <120669161+ifrahnz26@users.noreply.github.com>
2025-05-08 12:16:38 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to SpaceHub!</h1>
+        <p>This is the frontend React app for the MERN stack.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
